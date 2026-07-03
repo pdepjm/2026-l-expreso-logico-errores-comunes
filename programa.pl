18a7d4365bd44b647fe7ca8042bcb5686f0f9c93
tuqui <silvanad946@gmail.com>
2026-07-02 22:56:15 -0300

    Errores comunes

--- programa.pl
+++ programa.pl
@@ -4,22 +4,17 @@ horaMuerte(24).
 %dosisEncontrada/1 -> DosisEnMg
 dosisEncontrada(50).
 
+% 1
+
 horaDeAdministracion(Hora):-
     horaMuerte(HoraMuerte),
     dosisEncontrada(Dosis),
     Hora is HoraMuerte - 100 / Dosis.
 
-horaCritica(Hora, HoraMuerte, HoraAdmin):-
-    horaMuerte(HoraMuerte),
-    horaDeAdministracion(HoraAdmin),
-    between(HoraAdmin, HoraMuerte, Hora).
-
-
 horaDeAdministracion(hora):-
     horaMuerte(horaMuerte),
     dosisEncontrada(dosis),
     hora is horaMuerte - 100 / dosis.
-
 
 horaDeAdministracion(Hora):-
     Hora is 24 - 100 / 50.
@@ -39,18 +34,21 @@ horaDeAdministracion(Hora):-
 horaDeAdministracion(Hora):-
     Hora is horaMuerte - 100 / dosis.
 
+% 2
+
+horaCritica(Hora, HoraMuerte, HoraAdmin):-
+    horaMuerte(HoraMuerte),
+    horaDeAdministracion(HoraAdmin),
+    between(HoraAdmin, HoraMuerte, Hora).
+
 horasCriticas(Horas):-
     findall(Hora, esCritica(Hora), Horas).
-
-
 
 /*
 horaDeAdministracion(Hora):-
     horaMuerte(HoraMuerte),
     dosisEncontrada(Dosis),
     Hora is HoraMuerte - 100 / Dosis.
-
-% 2
 
 horasCriticas(Horas):-
     horaMuerte(HoraMuerte),
